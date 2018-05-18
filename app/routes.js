958b5429781e24cfe407de6173b7ca80ca5bfaf0
Gavin Wye <gavin@gavinwye.com>
2018-05-18 16:54:29 +0200

    first few pages

--- app/routes.js
+++ app/routes.js
@@ -9,6 +9,26 @@ router.get('/', function (req, res) {
 // Add your routes here - above the module.exports line
 
 // Data
+
+// Branching example
+router.get('/who-is-making-the-application', function (req, res) {
+  res.render('who-is-making-the-application')
+})
+
+router.get('/applied-for-compensation', function (req, res) {
+  // Get the answer from the query string (eg. ?over18=false)
+  if (req.session.data.applicationRep === 'false') {
+    // Redirect to the relevant page
+    res.redirect('applied-for-compensation')
+  } else {
+    // If over18 is any other value (or is missing) render the page requested
+    res.render('prototype')
+  }
+})
+
+
+
+// Tests
 router.get('/tests/data-test', function (req, res) {
   res.render('tests/data-test', { 'name': 'Gavin' })
 })
