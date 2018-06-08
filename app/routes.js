da7d0148ce28cb95d1bb7548fd94b7de304ef3cf
Gavin Wye <gavin@gavinwye.com>
2018-06-08 10:50:59 +0100

    removed £50 medical question from this journey

--- app/routes.js
+++ app/routes.js
@@ -236,7 +236,7 @@ router.post('/application/obtain-medical', function (req, res) {
 // File: obtain-medical
 // Variable: obtainMedical
 
-router.post('/application/pay-for-medical', function (req, res) {
+router.post('/application/unable-to-work', function (req, res) {
   // Get the answer from the query string (eg. ?over18=false)
   var obtainMedical = req.session.data['obtainMedical']
 
@@ -245,7 +245,7 @@ router.post('/application/pay-for-medical', function (req, res) {
     res.redirect('/application/prototype')
   } else {
     // If over18 is any other value (or is missing) render the page requested
-    res.redirect('/application/pay-for-medical')
+    res.redirect('/application/unable-to-work')
   }
 })
 
@@ -255,18 +255,18 @@ router.post('/application/pay-for-medical', function (req, res) {
 // File: pay-for-medical
 // Variable: obtainMedical
 
-router.post('/application/unable-to-work', function (req, res) {
-  // Get the answer from the query string (eg. ?over18=false)
-  var payforMedical = req.session.data['payforMedical']
-
-  if (payforMedical === 'no') {
-    // Redirect to the relevant page
-    res.redirect('/application/prototype')
-  } else {
-    // If over18 is any other value (or is missing) render the page requested
-    res.redirect('/application/unable-to-work')
-  }
-})
+// router.post('/application/unable-to-work', function (req, res) {
+//   // Get the answer from the query string (eg. ?over18=false)
+//   var payforMedical = req.session.data['payforMedical']
+//
+//   if (payforMedical === 'no') {
+//     // Redirect to the relevant page
+//     res.redirect('/application/prototype')
+//   } else {
+//     // If over18 is any other value (or is missing) render the page requested
+//     res.redirect('/application/unable-to-work')
+//   }
+// })
 
 // END__######################################################################################################
 
