b77dd2b97c72abb28675316bbb568c0d0154b522
stephanie <stephanie.krus@cica.gsi.gov.uk>
2018-07-13 17:31:16 +0100

    add review confirmation screen and route for it

--- app/routes.js
+++ app/routes.js
@@ -190,6 +190,25 @@ router.post('/application/OCJ-service-option-4', function (req, res) {
 // END__######################################################################################################
 
 // START__####################################################################################################
+// File: confirmation-page-if-automatic-nil
+// variable: apply-for-review
+
+router.post('/application/confirmation-page-if-automatic-nil', function (req, res) {
+  // Get the answer from the query string 
+  var applyForReview = req.session.data['apply-for-review']
+
+  if (applyForReview  === 'yes') {
+    // Redirect to the relevant page
+    res.redirect('/application/confirmation-of-review')
+  } else {
+    // If the variable is any other value (or is missing) render the page requested
+    res.redirect('/application/prototype')
+  }
+})
+
+// END__######################################################################################################
+
+// START__####################################################################################################
 // File: incident-reported
 // Variable: incidentReported
 
