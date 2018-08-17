4c3268f08d0fcc883129f2ffbffae88b79efb2e1
stephanie <stephanie.krus@cica.gsi.gov.uk>
2018-08-17 12:06:51 +0100

    change the non uk option in the postcode to a single textbox

--- app/routes.js
+++ app/routes.js
@@ -341,7 +341,8 @@ router.post('/application/email-address', function (req, res) {
 // START__####################################################################################################
 // File: address
 // this is the first step of getting an address - from here either the user select 'Find UK address' and goes to the view 'find-uk-address' 
-// or uses the link in the reveal to enter the address manually and goes to the view 'address-manually'
+// or uses the link in the reveal to enter the address manually and goes to the view 'address-manually' 
+// or  uses the link in the reveal to enter an on uk address and goes to the view 'address-non-UK'
 router.post('/application/address', function (req, res) {
   res.redirect('/application/find-uk-address')
 })
@@ -349,7 +350,7 @@ router.post('/application/address', function (req, res) {
 
 // START__####################################################################################################
 // File: find-uk-address 
-// will produce an address on one line one the 'check your answers page'
+// will produce an address on one line on the 'check your answers page'
 router.post('/application/find-uk-address', function (req, res) {
   if (req.session.checking_answers) { //the user was coming from the check your answer page, we are returning them there
     return res.redirect('/application/check-your-answers-page')
@@ -360,8 +361,22 @@ router.post('/application/find-uk-address', function (req, res) {
 
 // START__####################################################################################################
 // File: address-manually
-// will produce an address on multiple line on the 'check your answers page'
+// will produce an address on multiple lines on the 'check your answers page'
 router.post('/application/address-manually', function (req, res) {
+  req.session.data['address-non-UK'] = null; // this line is here to clear the data if the user had used the non UK address initially as we don't want to have duplicated data in the end
+  req.session.data['address-lookup-result'] = null; // this line is here to clear the data if the user had used the postcode look-up initially as we don't want to have duplicated data in the end
+  if (req.session.checking_answers) { //the user was coming from the check your answer page, we are returning them there
+    return res.redirect('/application/check-your-answers-page')
+  }
+  res.redirect('/application/phone-number')
+})
+// END__######################################################################################################
+
+// START__####################################################################################################
+// File: address-non-UK
+// will produce an address as text on the 'check your answers page'
+router.post('/application/address-non-UK', function (req, res) {
+  req.session.data['address-manually'] = null; // this line is here to clear the data if the user had used the option to enter an address manually initially as we don't want to have duplicated data in the end
   req.session.data['address-lookup-result'] = null; // this line is here to clear the data if the user had used the postcode look-up initially as we don't want to have duplicated data in the end
   if (req.session.checking_answers) { //the user was coming from the check your answer page, we are returning them there
     return res.redirect('/application/check-your-answers-page')
