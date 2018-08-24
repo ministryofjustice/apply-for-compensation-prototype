069e062b083e36fea780a66627e9afa25b310acf
johnkane1 <34911484+johnkane1@users.noreply.github.com>
2018-08-24 10:15:21 +0100

    updated crime not reported screen and routing

--- app/routes.js
+++ app/routes.js
@@ -205,6 +205,18 @@ router.post('/application/incident-reported', function (req, res) {
 // END__######################################################################################################
 
 // START__####################################################################################################
+// File: Reporting-crime-not-reported
+// Variable: none
+
+router.post('/application/reporting-crime-not-reported', function (req, res) {
+  if (req.session.checking_answers) { //the user was coming from the check your answer page, we are returning them there
+    return res.redirect('/application/check-your-answers-page')
+  }
+  res.redirect('/application/name')
+})
+// END__######################################################################################################
+
+// START__####################################################################################################
 // File: crime-reported-date
 //
 router.post('/application/crime-reported-date', function (req, res) {
