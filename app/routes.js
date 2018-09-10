f40be959206d49b2070d82eaaa69fcc3ee205713
Gavin Wye <gavin@gavinwye.com>
2018-09-10 15:57:32 +0200

    changed other compensation to go to check your answers

--- app/routes.js
+++ app/routes.js
@@ -454,7 +454,7 @@ router.post('/application/compensation-why-not', function (req, res) {
   if (req.session.checking_answers) { //the user was coming from the check your answer page, we are returning them there
     return res.redirect('/application/check-your-answers-page')
   }
-    res.redirect('/application/name')
+    res.redirect('/application/check-your-answers-page')
 })
 // END__######################################################################################################
 
