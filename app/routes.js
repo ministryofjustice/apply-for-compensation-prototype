b09e66a2f0f24c471116676946fa079a9167c306
Gavin Wye <gavin@gavinwye.com>
2018-09-10 15:52:50 +0200

    added routing to tak you back to the check your answers
    if you have come from there and edited an answer

--- app/routes.js
+++ app/routes.js
@@ -875,6 +875,10 @@ router.post('/application/ongoing-relationship', function (req, res) {
 // File: what-is-relationship
 //
 router.post('/application/what-is-relationship', function (req, res) {
+
+  if(req.session.checking_answers) {
+    return res.redirect('/application/check-your-answers-page')
+  }
   res.redirect('/application/name')
 })
 // END__######################################################################################################
