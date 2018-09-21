fcaccb05d36ac86a11acfb459987a09cf7f6d1ac
Gavin Wye <gavin@gavinwye.com>
2018-09-21 17:01:30 +0200

    moved routes for after 1979

--- app/routes.js
+++ app/routes.js
@@ -51,23 +51,7 @@ router.get('/', function (req, res) {
 
 
 
-// START__####################################################################################################
-// File: after-1979
-// Variable: after1979
 
-router.post('/application/after-1979', function (req, res) {
-  // Get the answer from the query string
-  var after1979 = req.session.data['after1979']
-
-  if (after1979 === 'no') {
-    // Redirect to the relevant page
-    res.redirect('/application/same-family')
-  } else {
-    // If the variable is any other value (or is missing) render the page requested
-    res.redirect('/application/you-have-a-choice')
-  }
-})
-// END__######################################################################################################
 
 // START__####################################################################################################
 // File: same-family
@@ -919,3 +903,4 @@ require('./views/application/british-citizen/routes')(router);
 require('./views/application/over-18/routes')(router);
 require('./views/application/who-is-making-the-application/routes')(router);
 require('./views/application/sexual-assault-application/routes')(router);
+require('./views/application/after-1979/routes')(router);
