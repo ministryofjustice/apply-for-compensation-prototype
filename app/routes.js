581146fe65dcca07bf4852c2bbadd09cd9aa1837
Gavin Wye <gavin@gavinwye.com>
2018-09-21 21:31:17 +0200

    moved routes for find UK address

--- app/routes.js
+++ app/routes.js
@@ -28,16 +28,6 @@ router.get('/', function (req, res) {
 // Add your routes here - above the module.exports line
 
 
-// START__####################################################################################################
-// File: find-uk-address
-// will produce an address on one line on the 'check your answers page'
-router.post('/application/find-uk-address', function (req, res) {
-  if (req.session.checking_answers) { //the user was coming from the check your answer page, we are returning them there
-    return res.redirect('/application/check-your-answers-page')
-  }
-  res.redirect('/application/phone-number')
-})
-// END__######################################################################################################
 
 
 
