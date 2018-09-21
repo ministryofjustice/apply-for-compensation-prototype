487788923c6d61982aab4106b9df41829bb33e61
Gavin Wye <gavin@gavinwye.com>
2018-09-21 21:35:46 +0200

    moved routes for application delay

--- app/routes.js
+++ app/routes.js
@@ -30,41 +30,6 @@ router.get('/', function (req, res) {
 
 
 
-
-
-
-// START__####################################################################################################
-// File: how-much-compensation
-//
-router.post('/application/compensation-amount', function (req, res) {
-  if (req.session.checking_answers) { //the user was coming from the check your answer page, we are returning them there
-    return res.redirect('/application/check-your-answers-page')
-  }
-    res.redirect('/application/single-or-multiple-incidents')
-})
-// END__######################################################################################################
-
-
-
-
-
-
-
-
-
-// START__####################################################################################################
-// File: application-delay
-// we see that screen if the indicent date (or when the incident stopped for POA) is over 2 years from the date of application
-//logic for it is for incident-date and period-of-abuse-end
-
-router.post('/application/application-delay', function (req, res) {
-  if (req.session.checking_answers) { //the user was coming from the check your answer page, we are returning them there
-    return res.redirect('/application/check-your-answers-page')
-  }
-  res.redirect('/application/incident-location')
-})
-// END__######################################################################################################
-
 // START__####################################################################################################
 // File: previous-applications
 // Variable: previous-applications
