b6e264d39d2c4b3010a70ac239bd233af5dde3d5
Gavin Wye <gavin@gavinwye.com>
2018-09-21 21:55:39 +0200

    added more routes to address

--- app/views/application/address/routes.js
+++ app/views/application/address/routes.js
@@ -8,4 +8,15 @@ module.exports = function (router) {
     res.redirect('/application/find-uk-address')
   })
   // END__######################################################################################################
+
+  // START__####################################################################################################
+  // File: address
+  //
+  router.post('/application/address', function (req, res) {
+    if (req.session.checking_answers) { //the user was coming from the check your answer page, we are returning them there
+      return res.redirect('/application/check-your-answers-page')
+    }
+    res.redirect('/application/phone-number')
+  })
+  // END__######################################################################################################
 }
