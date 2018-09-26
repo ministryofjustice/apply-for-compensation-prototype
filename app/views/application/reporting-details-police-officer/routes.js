1aea976142df0b1b339e3fdcb5ed92171d025cbe
Gavin Wye <gavin@gavinwye.com>
2018-09-26 19:40:03 +0100

    Added the question variable to the police officer question

--- app/views/application/reporting-details-police-officer/routes.js
+++ app/views/application/reporting-details-police-officer/routes.js
@@ -5,5 +5,9 @@ module.exports = function (router) {
   router.post('/application/reporting-details-police-officer', function (req, res) {
    res.redirect('/application/crime-reference')
   })
+  // Pass the question in to the page
+  router.get('/application/reporting-details-police-officer/', function (req, res) {
+    res.render('application/reporting-details-police-officer/index', { 'policeOfficerQuestion': 'What are the details of the police officer who recorded the crime?' })
+  })
   // END__######################################################################################################
 }
