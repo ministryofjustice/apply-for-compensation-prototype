4076bc78aa93a81e916951a9f8c659adb6f290af
Gavin Wye <gavin@gavinwye.com>
2018-09-26 19:37:46 +0100

    Added the question variable to the police force question

--- app/views/application/reporting-details-what-force/routes.js
+++ app/views/application/reporting-details-what-force/routes.js
@@ -5,5 +5,10 @@ module.exports = function (router) {
   router.post('/application/reporting-details-what-force', function (req, res) {
    res.redirect('/application/reporting-details-police-officer')
   })
+
+  // Pass the question in to the page
+  router.get('/application/reporting-details-what-force/', function (req, res) {
+    res.render('application/reporting-details-what-force/index', { 'policeForceQuestion': 'What police force was the crime reported to?' })
+  })
   // END__######################################################################################################
 }
