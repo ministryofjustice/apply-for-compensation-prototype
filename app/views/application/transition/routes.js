c6a1aa2b54c1dd9d8c70e7772ba41861da0fd8f2
Gavin Wye <gavin@gavinwye.com>
2018-10-01 19:26:48 +0200

    made some design changes to the laypout of the page

--- app/views/application/transition/routes.js
+++ app/views/application/transition/routes.js
@@ -1,18 +1,17 @@
 module.exports = function (router, content) {
   // START__####################################################################################################
-  // File: tell-criminal-convictions
-  // this view is called if user says yes to convicted of a criminal offence
+  // File: transition
 
-  router.post('/application/tell-criminal-convictions', function (req, res) {
+  router.post('/application/transition', function (req, res) {
     if (req.session.checking_answers) { //the user was coming from the check your answer page, we are returning them there
       return res.redirect('/application/check-your-answers-page')
     }
-    res.redirect('/application/bridge')
+    res.redirect('https://www.cica.gov.uk/OAS/Account/Create')
   })
-  
+
   // Pass the question in to the page
-  router.get('/application/tell-criminal-convictions/', function (req, res) {
-    res.render('application/tell-criminal-convictions/index', content)
+  router.get('/application/transition/', function (req, res) {
+    res.render('application/transition/index', content)
   })
 
   // END__######################################################################################################
