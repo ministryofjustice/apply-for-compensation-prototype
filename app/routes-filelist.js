0b33340206ba41170f0538c1166c54ffe91f4f4e
Adrian Roworth <adrian.roworth@CICA.GOV.UK>
2018-10-01 12:57:15 +0100

    Update listing logic

--- app/routes-filelist.js
+++ app/routes-filelist.js
@@ -1,33 +1,49 @@
+const path = require('path');
 module.exports = function (router) {
 
     var fs = require('fs');
 
     router.get('/file-list', function (req, res) {
+        let filesList;
 
-        function markup(files, callback) {
-
-            var items = [];
-            for (i = 0; i < files.length; i++) {
-                var q = files[i];
-                // q = q.slice(0, -5);
-                var markup = '<li>' + '<a href="/application/' + q + '">' + q + '</a>' + '</li>';
-                items.push(markup);
-            };
-            callback(items);
-        };
-
-        var getFiles = function (callback) {
-            fs.readdir(__dirname + '/views/application/', function (err, files) { // '/' denotes the root folder
-                if (err) {
-                    throw err;
+        // List all files in a directory recursively.
+        var walkSync = function (dir, filelist) {
+            var fs = fs || require('fs'),
+                files = fs.readdirSync(dir);
+            filelist = filelist || [];
+            files.forEach(function (file) {
+                if (fs.statSync(path.join(dir, file)).isDirectory()) {
+                    filelist = walkSync(path.join(dir, file), filelist);
                 }
-                markup(files, callback)
+                else {
+                    filelist.push(path.join(dir, file));
+                }
             });
+            return filelist;
         };
 
-        getFiles(function (items) {
+        function markup(filePaths) {
+            let htmlList = [];
+
+            filePaths.forEach(filePath => {
+                const newPath = filePath.substring(filePath.indexOf('views') + 5).replace(/\\/g, '/');
+                newPath.replace('/index.html', '');
+                const markup = '<li>' + '<a href="' + newPath + '">' + newPath + '</a>' + '</li>';
+                htmlList.push(markup);
+            });
+
+            return htmlList
+        };
+
+        var getFilesList = function (callback) {
+            filesList = walkSync(path.join(__dirname, '/views/'), []);
+            filesList = filesList.filter(filePath => filePath.endsWith('.html'));
+            callback && callback(filesList);
+        };
+
+        getFilesList(function (files) {
             // render markup for items
-            res.render('file-list', { 'files': items });
+            res.render('file-list', { 'files': markup(files) });
         });
     });
 }
