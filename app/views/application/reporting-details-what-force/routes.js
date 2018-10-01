77f881c10f6d04b729f0e001bc475ede0ac7eb01
Gavin Wye <gavin@gavinwye.com>
2018-10-01 17:02:30 +0200

    fixed broken route for police officer details

--- app/views/application/reporting-details-what-force/routes.js
+++ app/views/application/reporting-details-what-force/routes.js
@@ -3,9 +3,9 @@ module.exports = function (router, content) {
   // File: reporting-details-what-force
   //
   router.post('/application/reporting-details-what-force', function (req, res) {
-   res.redirect('/application/reporting-details-what-force')
+   res.redirect('/application/reporting-details-police-officer')
   })
-  
+
   // Pass the question in to the page
   router.get('/application/reporting-details-what-force/', function (req, res) {
     res.render('application/reporting-details-what-force/index', content)
