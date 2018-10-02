786cc7324af9a095c0dcfd799b0978421c188173
Gavin Wye <gavin@gavinwye.com>
2018-10-02 12:40:46 +0100

    removed duplicate content variables and sorted the list

--- app/routes-content.js
+++ app/routes-content.js
@@ -2,126 +2,86 @@
 // // START__####################################################################################################
 //
 // Add content key/value pairs here
-// Add content key/value pairs here
-// Content variables are isted in the same order as folders when sorted a-z
-// If a variable has been defined in a previous file it is not defined again
-// Comments sepperate the list
-// This list should be moved in to it's own file in the future
+// The list os sorted a-z use search to find the variable you need
+// Variable names should be fairly self explanitory and it should be easy to understand what
+// file they are used in. If in doubt serch is your friend!
+// Some variables are used on more than one page (that's the point of having them like this)
+// be carful when changing them use a global find to make sure that you are aware of all of the
+// pages that will be changed
 module.exports = {
-    // Postcode lookup
-    addressQuestion: 'Enter your address',
-    postcodeLabel: 'Postcode',
-    // Manual address entry
-    addressQuestion: 'Enter your address',
     addressLine1Label: 'Building and street <span class="govuk-visually-hidden">line 1 of 2</span>',
     addressLine2Label: '<span class="govuk-visually-hidden">Building and street line 2 of 2</span>',
-    townOrCityLabel: 'Town or city',
-    countyLabel: 'County (optional)',
-    postcodeOptionalLabel: 'Postcode (optional)',
-    // Non uk address
     addressQuestion: 'Enter your address',
-    // After 1979
     after1979Question: 'Did the crime happen on or after 1st October 1979?',
-    // Application delay
-    whyDelayQuestion: 'Why did you not apply to us for compensation sooner?',
-    // British citizen
     britishCitizenQuestion: 'Are you a British citizen?',
-    // Compensation
-    otherCompensationQuestion: 'Have you applied for any other compensation for this crime?',
-    otherCompensationHint: 'Other compensation can include, for example, insurance, civil damages and court compensation.',
-    // Compensation amount
-    howMuchCompensationQuestion: 'How much compensation did you get?',
-    howMuchCompensationHint: 'You can also tell us if you are still waiting to find out.',
-    // Who have you applied to for compensation
-    whoCompensationQuestion: 'Who else have you applied to for compensation?',
-    // Compensation why not
     compensationWhyNotQuestion: "Why did you not apply for compensation from another source?",
-    // Crime reference number
-    crimeReferenceNumberQuestion: 'What\'s the crime reference number?',
+    countyLabel: 'County (optional)',
     crimeReferenceNumberHint: 'For example, 31 3 2018. You can enter an approximate date',
-    // Crime reporting date
-    crimeReportedDateQuestion: 'When was the crime reported to police?',
+    crimeReferenceNumberQuestion: 'What\'s the crime reference number?',
     crimeReportedDateHint: 'For example, 31 3 2018. You can enter an approximate date',
-    // Criminal convictions
-    criminalConvictionsQuestion: 'Have you ever been convicted of a crime?',
+    crimeReportedDateQuestion: 'When was the crime reported to police?',
     criminalConvictionsHint: 'You do not need to declare convictions that are spent',
-    // Date of birth
+    criminalConvictionsQuestion: 'Have you ever been convicted of a crime?',
     dateOfBirthQuestion: "Enter your date of birth",
-    errorPastMessage: "Your date of birth must be in the past",
-    errorIncompleteMessage: "Enter your date of birth and include a day, month and year",
-    errorBeforeIncident: "Your date of birth must be before the crime happened",
-    // Declaration
-    declarationHeading: 'Declaration',
     declarationBody: 'By continuing you confirm that the information you will give is true as far as you know.',
-    warningText: "You could be prosecuted or get less compensation if you give false or misleading information.",
+    declarationHeading: 'Declaration',
     declarationQuestion: 'Do you agree?',
-    // Do you know offender
     doYouKnowOfdfenderQuestion: "Do you know the name of the person who attacked you?",
-    // Email address
-    emailAddressQuestion: "Enter your email address",
-    emailAddressHint: "We'll use this to contact you about your application for example, to request more information and tell you about our decision",
-    emailAddressErrorSubHeader: "Enter your email address?",
     emailAddressError: "Enter your email address, for example john.smith@email.com",
-    // Find uk addresse
-    selectAddressLable: 'Select an address',
-    // Incident date
+    emailAddressErrorSubHeader: "Enter your email address?",
+    emailAddressHint: "We'll use this to contact you about your application for example, to request more information and tell you about our decision",
+    emailAddressQuestion: "Enter your email address",
+    errorBeforeIncident: "Your date of birth must be before the crime happened",
+    errorIncompleteMessage: "Enter your date of birth and include a day, month and year",
+    errorMessageContent: 'Select Once or Over a period of time',
+    errorMessageTitle: 'There is a problem',
+    errorPastMessage: "Your date of birth must be in the past",
+    howMuchCompensationHint: 'You can also tell us if you are still waiting to find out.',
+    howMuchCompensationQuestion: 'How much compensation did you get?',
     incidentDateHeading: "When did the crime happen?",
     incidentDateHint: 'For example, 31 3 2018. You can enter an approximate date ',
-    // Incident location
     incidentLocationHeading: "Where did the crime happen?",
     incidentLocationOther: "Tell us where the crime happened",
-    // Incident reported
     incidentReportedQuestion: 'Was the crime reported to the police?',
-    // Name
-    nameQuestion: 'Enter your name',
-    nameErrorNoTitle: "Enter your title",
-    nameErrorNoFirstName: "Enter your first name",
-    nameErrorNoLastName: "Enter your last name",
-    // Name have other
-    otherNameQuestion: 'Have you ever been known by any other names?',
-    // Name other
-    nameOtherQuestion: "What other names have you used?",
-    otherNameError: "Select yes if you have been known by any other names",
-    // Offender name
-    offenderNameQuestion: 'Enter their name',
-    // Ongoing relationship
-    ongoingRelationshipQuestion: "Do you have any contact with this person?",
-    // Over 18
-    over18Question: 'Are you 18 or over?',
-    // Period of abuse start and end
-    periodOfAbuseStartQuestion: 'When did it start?',
-    periodOfAbuseEndQuestion: 'When did it stop?',
-    periodOfAbuseHint: 'For example, 03 2018. You can enter an approximate date',
-    // Phone number
-    phoneNumberQuestion: "Enter your telephone number",
-    phoneNumberHint: "We'll use this to contact you about your application for example, to request more information",
-    // Previous applications
-    previousApplicationsQuestion: 'Have you applied for compensation from us for this crime before?',
-    // Reporting delay
-    reportingDelayQuestion: "Why was the crime not reported sooner?",
-    // Police officer details
-    policeOfficerQuestion: 'What are the details of the police officer who recorded the crime?',
-    policeOfficerName: 'Name',
-    policeOfficerIdNumber: 'ID number (collar number)',
-    // Police force
-    policeForceQuestion: 'What police force was the crime reported to?',
-    policeForceHint: "Enter the police force name, for example, Humberside police.",
-    // Same family
-    sameFamilyQuestion: 'Were you and your attacker living together as family at the time?',
-    // Sexual assult application
-    sexualAbuseQuestion: 'Are you a victim of sexual violence or abuse?',
-    // Single or multiple incidents
-    singleOrMultipleIncidentsQuestion: 'Did the crime happen once or over a period of time?',
     itemOneValue: 'Once',
     itemTwoValue: 'Over a period of time',
-    errorMessageTitle: 'There is a problem',
-    errorMessageContent: 'Select Once or Over a period of time',
-    // Criminal convictions
+    nameErrorNoFirstName: "Enter your first name",
+    nameErrorNoLastName: "Enter your last name",
+    nameErrorNoTitle: "Enter your title",
+    nameOtherQuestion: "What other names have you used?",
+    nameQuestion: 'Enter your name',
+    offenderNameQuestion: 'Enter their name',
+    ongoingRelationshipQuestion: "Do you have any contact with this person?",
+    otherCompensationHint: 'Other compensation can include, for example, insurance, civil damages and court compensation.',
+    otherCompensationQuestion: 'Have you applied for any other compensation for this crime?',
+    otherNameError: "Select yes if you have been known by any other names",
+    otherNameQuestion: 'Have you ever been known by any other names?',
+    over18Question: 'Are you 18 or over?',
+    periodOfAbuseEndQuestion: 'When did it stop?',
+    periodOfAbuseHint: 'For example, 03 2018. You can enter an approximate date',
+    periodOfAbuseStartQuestion: 'When did it start?',
+    phoneNumberHint: "We'll use this to contact you about your application for example, to request more information",
+    phoneNumberQuestion: "Enter your telephone number",
+    policeForceHint: "Enter the police force name, for example, Humberside police.",
+    policeForceQuestion: 'What police force was the crime reported to?',
+    policeOfficerIdNumber: 'ID number (collar number)',
+    policeOfficerName: 'Name',
+    policeOfficerQuestion: 'What are the details of the police officer who recorded the crime?',
+    postcodeLabel: 'Postcode',
+    postcodeOptionalLabel: 'Postcode (optional)',
+    previousApplicationsQuestion: 'Have you applied for compensation from us for this crime before?',
+    reportingDelayQuestion: "Why was the crime not reported sooner?",
+    sameFamilyQuestion: 'Were you and your attacker living together as family at the time?',
+    selectAddressLable: 'Select an address',
+    sexualAbuseQuestion: 'Are you a victim of sexual violence or abuse?',
+    singleOrMultipleIncidentsQuestion: 'Did the crime happen once or over a period of time?',
     tellCriminalConvictionsQuestion: 'Tell us about your convictions',
-    // What is relationship
+    townOrCityLabel: 'Town or city',
+    warningText: "You could be prosecuted or get less compensation if you give false or misleading information.",
     whatIsRelationshipQuestion: "What type of contact do you have with them?",
-    // Who is making the application
-    whoIsMakingTheApplicationQuestion: 'Who are you applying for?'
+    whoCompensationQuestion: 'Who else have you applied to for compensation?',
+    whoIsMakingTheApplicationQuestion: 'Who are you applying for?',
+    whyDelayQuestion: 'Why did you not apply to us for compensation sooner?',
 };
 // // END__######################################################################################################
 // }
