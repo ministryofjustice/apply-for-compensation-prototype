acd72ec629c69b1aafe24ad8a0234f315b948916
johnkane1 <34911484+johnkane1@users.noreply.github.com>
2018-11-01 15:00:43 +0000

    content changes

--- app/views/application/OCJ-result/routes.js
+++ app/views/application/OCJ-result/routes.js
@@ -7,7 +7,7 @@ module.exports = function (router, content) {
     // Get the answer from the query string
     var OCJresult = req.session.data['OCJresult']
 
-    if (OCJresult === 'No') {
+    if (OCJresult === 'OCJ') {
       // Redirect to the relevant page
       res.redirect('/application/declaration')
     } else {
