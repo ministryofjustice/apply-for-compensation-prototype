2126ba0f2862254d51004192920c89550f929203
Gavin Wye <gavin@gavinwye.com>
2018-12-03 18:40:40 +0100

    Added the add another behaviour

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,4 +7,34 @@ if (window.console && window.console.info) {
 
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
+
+  // This adds another of the compoent defined after the id of the same thing
+    $("#addAnother").click(function(){
+      $("#offenderName").after(
+        '<h2 class="govuk-heading-m">Offender 2</h2>' +
+        '<div id="offenderName">' +
+          '<div class="govuk-form-group">' +
+            '<label class="govuk-label" for="name">' +
+              'First name' +
+            '</label>' +
+            '<input class="govuk-input" id="name" name="name" type="text">' +
+          '</div>' +
+          '<div class="govuk-form-group">' +
+            '<label class="govuk-label" for="name">' +
+              'Last name' +
+            '</label>' +
+            '<input class="govuk-input" id="name" name="name" type="text">' +
+          '</div>' +
+        '</div>' +
+        '<p><a href="#" id="#removeOffender">Remove offender</a></p>'
+      );
+    });
+
+    // remove a prisons
+    $(document).on('click', '#removeOffender', function (e) {
+      e.preventDefault();
+      $(this).parents('#offenderName').remove();
+      // sortParentFields();
+    });
+
 })
