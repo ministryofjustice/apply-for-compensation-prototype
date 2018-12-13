fcd65436d8fce84e9582d0d00fc2d1a15c96b633
johnkane1 <34911484+johnkane1@users.noreply.github.com>
2018-12-13 15:05:57 +0000

    MVP-552 Previous applications

--- app/views/application/previous-applications/routes.js
+++ app/views/application/previous-applications/routes.js
@@ -15,10 +15,10 @@ module.exports = function (router, content) {
       if (req.session.checking_answers) { //the user was coming from the check your answer page, we are returning them there
         return res.redirect('/application/check-your-answers-page')
       }
-      res.redirect('/application/incident-location')
+      res.redirect('/application/compensation')
     }
   })
-  
+
   // Pass the question in to the page
   router.get('/application/previous-applications/', function (req, res) {
     res.render('application/previous-applications/index', content)
