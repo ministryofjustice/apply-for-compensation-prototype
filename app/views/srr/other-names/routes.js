571e190ef0b1f404cc00ef24e9488cd2f95a61e1
Maxine Divers <maxinedivers@Maxines-MacBook-Pro.local>
2019-03-25 16:28:39 +0000

    SRR prototype draft done
    Including John's content

--- app/views/srr/other-names/routes.js
+++ app/views/srr/other-names/routes.js
@@ -4,18 +4,15 @@ module.exports = function (router, content) {
   //
   router.post('/srr/other-names', function (req, res) {
     if (req.session.checking_answers) { //the user was coming from the check your answer page, we are returning them there
-      return res.redirect('/srr/other-names')
+      return res.redirect('/srr/check-your-answers-page')
     }
     res.redirect('/srr/date-of-birth')
   })
 
   // Pass the question in to the page
-  router.get('/srr/other-names', function (req, res) {
-    res.render('/srr/other-names/index', content)
+  router.get('/srr/other-names/', function (req, res) {
+    res.render('srr/other-names/index', content)
   })
-  // Pass the question in to the page
-  router.get('/srr/other-names/error', function (req, res) {
-    res.render('/srr/other-names/error', content)
-  })
+
   // END__######################################################################################################
 }
--- app/views/srr/phone-number/routes.js
+++ app/views/srr/phone-number/routes.js
@@ -4,7 +4,7 @@ module.exports = function (router, content) {
   //
   router.post('/srr/phone-number', function (req, res) {
     if (req.session.checking_answers) { //the user was coming from the check your answer page, we are returning them there
-      return res.redirect('/application/check-your-answers-page')
+      return res.redirect('/srr/check-your-answers-page')
     }
     res.redirect('/srr/preferred-days')
   })
