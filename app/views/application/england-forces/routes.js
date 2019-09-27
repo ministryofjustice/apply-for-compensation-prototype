9ce25ea13083dc616dd0eed32e139cecf251576a
Adam Silver <adambsilver@gmail.com>
2019-09-27 08:32:22 +0100

    Fix hanging request

--- app/views/application/england-forces/routes.js
+++ app/views/application/england-forces/routes.js
@@ -5,6 +5,7 @@ module.exports = function (router, content) {
 // Variable: britishCitizen
 
 router.post('/application/england-forces', function (req, res) {
+
   // Get the answer from the query string
   var incidentDateDay = req.session.data['incident-date-day']
   var incidentDateMonth = req.session.data['incident-date-month']
@@ -12,35 +13,43 @@ router.post('/application/england-forces', function (req, res) {
   var incidentDate;
 
   if (req.session.checking_answers) { //the user was coming from the check your answer page, we are returning them there
+
     return res.redirect('/application/check-your-answers-page')
+  }
+
+  if (req.session.data['period-of-abuse-end-month']) { // this is a period of abuse and we have the data for the end date (month and year)
+
+    var POAEndMonth = req.session.data['period-of-abuse-end-month']
+    var POAEndYear = req.session.data['period-of-abuse-end-year']
+    var reportingDateDay = req.session.data['incidentReported-day']
+    var reportingDateMonth = req.session.data['incidentReported-month']
+    var reportingDateYear = req.session.data['incidentReported-year']
+    var reportingDate = dateHelper.getDatefrom3inputs(reportingDateDay,reportingDateMonth, reportingDateYear) //create a date that is the report date from the 3 elements we received from the user
+    incidentDate = dateHelper.getDatefrom2inputs(POAEndMonth, POAEndYear) // we need the date  of the last day of the month of the end of the period of abuse to compare for delay reporting over 48h
+
+    if ( dateHelper.isReportedOver48h(incidentDate, reportingDate)){ // changing the incident date is now triggering the reporting delay screen
+
+      return res.redirect('/application/reporting-delay')
     }
+  }
 
-    if (req.session.data['period-of-abuse-end-month']) { // this is a period of abuse and we have the data for the end date (month and year)
-      var POAEndMonth = req.session.data['period-of-abuse-end-month']
-      var POAEndYear = req.session.data['period-of-abuse-end-year']
-      var reportingDateDay = req.session.data['incidentReported-day']
-      var reportingDateMonth = req.session.data['incidentReported-month']
-      var reportingDateYear = req.session.data['incidentReported-year']
-      var reportingDate = dateHelper.getDatefrom3inputs(reportingDateDay,reportingDateMonth, reportingDateYear) //create a date that is the report date from the 3 elements we received from the user
-      incidentDate = dateHelper.getDatefrom2inputs(POAEndMonth, POAEndYear) // we need the date  of the last day of the month of the end of the period of abuse to compare for delay reporting over 48h
+  if (req.session.data['incidentReported-day']) { //we have the data for the day the crime was reported (so we must have come back here from the 'Change' on the check your answer page as it's out of sequence)
 
-      if ( dateHelper.isReportedOver48h(incidentDate, reportingDate)){ // changing the incident date is now triggering the reporting delay screen
-        return res.redirect('/application/reporting-delay')
-      }
-    }
+    var reportingDateDay = req.session.data['incidentReported-day']
+    var reportingDateMonth = req.session.data['incidentReported-month']
+    var reportingDateYear = req.session.data['incidentReported-year']
 
-    if (req.session.data['incidentReported-day']) { //we have the data for the day the crime was reported (so we must have come back here from the 'Change' on the check your answer page as it's out of sequence)
-      var reportingDateDay = req.session.data['incidentReported-day']
-      var reportingDateMonth = req.session.data['incidentReported-month']
-      var reportingDateYear = req.session.data['incidentReported-year']
-      var reportingDate = dateHelper.getDatefrom3inputs(reportingDateDay,reportingDateMonth, reportingDateYear) //create a date that is the report date from the 3 elements we received from the user
-      incidentDate = dateHelper.getDatefrom3inputs(incidentDateDay, incidentDateMonth, incidentDateYear) // we need the incident date to compare for delay reporting over 48h
+    var reportingDate = dateHelper.getDatefrom3inputs(reportingDateDay,reportingDateMonth, reportingDateYear) //create a date that is the report date from the 3 elements we received from the user
 
-      if ( dateHelper.isReportedOver48h(incidentDate, reportingDate)){ // changing the incident date is now triggering the reporting delay screen
-        return res.redirect('/application/reporting-delay')
-      }
 
+    incidentDate = dateHelper.getDatefrom3inputs(incidentDateDay, incidentDateMonth, incidentDateYear) // we need the incident date to compare for delay reporting over 48h
+
+    if ( dateHelper.isReportedOver48h(incidentDate, reportingDate)){ // changing the incident date is now triggering the reporting delay screen
+      return res.redirect('/application/reporting-delay')
     } else {
+      res.redirect('/application/do-you-know-offender')
+    }
+  } else {
     // If the variable is any other value (or is missing) render the page requesteds
     res.redirect('/application/do-you-know-offender')
   }
