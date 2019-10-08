c499347273a7cacced25e42891155f87c2739bbf
Jen Thomson <hello@jenthomson.com>
2019-10-08 17:16:01 +0100

    Adding in a police forces mockup
    Added one of the mockups created for choosing the police force investigating the crime. Using the accessible autocomplete.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,3 +9,60 @@ $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 
 })
+
+
+$(document).ready(function(){
+    $("a.govuk-button").click(function(){
+        var radioValue = $("[type='radio']:checked").val();
+
+        /// Is it the local force?
+
+        if(radioValue == "local-force"){
+          window.location.href = "/forces-mockups/v1/offender/";
+        }
+        if(radioValue == "other-england-force"){
+          window.location.href = "/forces-mockups/v1/england/forces/index";
+        }
+        if(radioValue == "scotland-force"){
+          window.location.href = "/forces-mockups/v1/scotland/forces/";
+        }
+        if(radioValue == "wales-force"){
+          window.location.href = "/forces-mockups/v1/wales/forces/";
+        }
+        if(radioValue == "british-transport-police"){
+          window.location.href = "/forces-mockups/v1/offender/";
+        }
+
+        /// Pick English area
+
+        if(radioValue == "crime-police-north-west"){
+          window.location.href = "/forces-mockups/v1/england/forces/north-west";
+        }
+        if(radioValue == "crime-police-north-east"){
+          window.location.href = "/forces-mockups/v1/england/forces/north-east";
+        }
+        if(radioValue == "crime-police-east-midlands"){
+          window.location.href = "/forces-mockups/v1/england/forces/east-midlands";
+        }
+        if(radioValue == "crime-police-eastern"){
+          window.location.href = "/forces-mockups/v1/england/forces/eastern";
+        }
+        if(radioValue == "crime-police-london"){
+          window.location.href = "/forces-mockups/v1/england/forces/london";
+        }
+        if(radioValue == "crime-police-south-east"){
+          window.location.href = "/forces-mockups/v1/england/forces/south-east";
+        }
+        if(radioValue == "crime-police-south-west"){
+          window.location.href = "/forces-mockups/v1/england/forces/south-west";
+        }
+        if(radioValue == "crime-police-west-midlands"){
+          window.location.href = "/forces-mockups/v1/england/forces/west-midlands";
+        }
+        if(radioValue == "crime-police-dont-know"){
+          window.location.href = "/forces-mockups/v1/england/forces/all";
+        }
+
+
+    });
+});
