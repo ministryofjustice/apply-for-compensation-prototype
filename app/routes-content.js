756451bb8d8197ad571b82e3bb11dec2e1702ba2
Colin Meney <colin.meney@digital.justice.gov.uk>
2019-12-03 12:06:14 +0000

    Minors and error messages
    - Added hint text to transition-over-18 page
    - Fixed broken link on error-pages
    - Changed minorTransitionOver18Heading

--- app/routes-content.js
+++ app/routes-content.js
@@ -177,7 +177,7 @@ module.exports = {
     minorAuthorityToApplyHeading:'Authority to apply',
     minorVictimAddressQuestion:'What is the child\'s address?',
     minorParentAddressQuestion:'What is your address?',
-    minorTransitionOver18Heading: 'You cannot apply for someone over 18 with this service',
+    minorTransitionOver18Heading: 'You cannot use this service to apply for someone over 18',
     minorAlsoChildAddressQuestion: 'Is this also the child\'s address?',
     minorChildAddressQuestion: 'Enter child\'s address',
     minorwhatIsRelationshipQuestion: "If your child has contact with the offender, describe it below",
