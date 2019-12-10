f00ee7e26e6194335795920179f4e2d3dbade49b
Jen Thomson <hello@jenthomson.com>
2019-12-10 17:20:48 +0000

    Concepts for 'Proof of parental responsibility'
    Added two options for 'Proof of parental responsibility'
    
    Option 1: shorter flow; provides context; gives option to upload or email/post; all uploads on one screen.
    
    Option 2: longer flow; provides context and gives option to upload or email/post; asks for birth certificate first; asks to confirm if names are the same as the document; prompts to submit further proof.
    
    Moved confirmation screen within the Minors folder.

--- app/routes-content.js
+++ app/routes-content.js
@@ -191,6 +191,12 @@ module.exports = {
     minorCompWho:'Who has your child applied to or received compensation from?',
     minorWhyDelayQuestion:'Select reasons for the delay in making your application?',
 
+    minorProofEligibilityUploadNowQuestion:'Do you want to upload your documents now?',
+    minorProofEligibilityNamesQuestion:'Does this document show your current names?',
+
+
+
+
     // Mental injuries questions //
     mentalHealthQuestion:'Has the crime affected your mental health so that you cannot do things you would normally do?',
     mentalHealthQuestionHint:'For example, going to work, seeing friends or being in a relationship.',
