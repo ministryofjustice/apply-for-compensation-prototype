b0ef68b506200315407f30da4473d010092f4579
Jen Thomson <hello@jenthomson.com>
2019-12-16 11:56:22 +0000

    Fixed issue with minors upload v2
    Prototype was not showing choice to upload screen

--- app/views/concepts/minors/eligibility-to-apply/option-2/upload-choice/routes.js
+++ app/views/concepts/minors/eligibility-to-apply/option-2/upload-choice/routes.js
@@ -4,13 +4,13 @@ module.exports = function (router, content) {
 // Variable: britishCitizen
 
 
-router.post('/concepts/minors/eligibility-to-apply/option-1/upload-choice', function (req, res) {
+router.post('/concepts/minors/eligibility-to-apply/option-2/upload-choice', function (req, res) {
   // Get the answer from the query string
   var minorUploadChoice = req.session.data['minorUploadChoice']
 
   if (minorUploadChoice === 'Yes') {
     // Redirect to the relevant page
-    res.redirect('/concepts/minors/eligibility-to-apply/option-1/upload')
+    res.redirect('/concepts/minors/eligibility-to-apply/option-2/upload')
   } else {
     if (req.session.checking_answers) { //the user was coming from the check your answer page, we are returning them there
       return res.redirect('/concepts/minors/check-your-answers-page')
@@ -22,8 +22,8 @@ router.post('/concepts/minors/eligibility-to-apply/option-1/upload-choice', func
 
 
 // Pass the question in to the page
-router.get('/concepts/minors/eligibility-to-apply/option-1/upload-choice', function (req, res) {
-  res.render('concepts/minors/eligibility-to-apply/option-1/upload-choice/index', content)
+router.get('/concepts/minors/eligibility-to-apply/option-2/upload-choice', function (req, res) {
+  res.render('concepts/minors/eligibility-to-apply/option-2/upload-choice/index', content)
 })
 
 router.get('/concepts/minors/v1/confirm-names/confirm-names-error', function (req, res) {
