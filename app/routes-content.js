d9eda58d0f63a83d2db5a21d51b9f9c7ad761291
Colin Meney <colin.meney@digital.justice.gov.uk>
2020-01-06 16:14:48 +0000

    SMS concept content
    Updates to question hints

--- app/routes-content.js
+++ app/routes-content.js
@@ -209,9 +209,9 @@ module.exports = {
 
     // SMS confirmation questions //
     smsMobileQuestion:'What is your mobile number?',
-    smsMobileNumberHint: "We may use this to contact you if we need to clarify something on your application form.",
+    smsMobileNumberHint: "We'll send a text message when we've received your application.",
     smsDoYouHaveEmailQuestion: "Do you have an email address?",
-    smsDoYouHaveEmailQuestionHint: "We may use this to contact you if we need to clarify something on your application form.",
+      smsDoYouHaveEmailQuestionHint: "We'll use this to tell you we've received your application.",
 
 
 };
