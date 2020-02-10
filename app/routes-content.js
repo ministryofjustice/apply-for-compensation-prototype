84465203a6821ac2cfe5cc038cb2ea5b72f96b2e
Colin Meney <colin.meney@digital.justice.gov.uk>
2020-02-10 09:38:57 +0000

    Mental injuries content update

--- app/routes-content.js
+++ app/routes-content.js
@@ -208,7 +208,7 @@ module.exports = {
 
     // Mental injuries questions //
     mentalHealthQuestion:'Do you have a disabling mental injury?',
-    mentalHealthQuestionHint:'This means it\'s much harder than usual to do things you would normally do, like go to work, see friends, or have a sexual relationship.',
+    mentalHealthQuestionHint:'This means it\'s much harder than usual to do things you would normally do, like going to work, seeing friends, or having a sexual relationship.',
     mentalHealthDurationQuestion:'Has your mental injury lasted more than 6 weeks?',
     mentalHealthDiagnosisQuestion:'Have you been diagnosed with a mental health condition as a result of the crime?',
     mentalHealthDiagnosisQuestionHint:'We may ask a clinical psychologist to look at your medical records if you’ve not been diagnosed yet. ',
