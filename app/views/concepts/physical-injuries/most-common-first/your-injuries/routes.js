3ccc240036dfb7bd0c0e4c7eaf2bc6cf120d904f
Jen <hello@jenthomson.com>
2020-05-27 10:04:20 +0100

    Most common injuries prototype
    Fixed link to end of prototype

--- app/views/concepts/physical-injuries/most-common-first/your-injuries/routes.js
+++ app/views/concepts/physical-injuries/most-common-first/your-injuries/routes.js
@@ -9,15 +9,6 @@ module.exports = function (router, content) {
     // Redirect to the relevant page
     res.redirect('/concepts/physical-injuries/one-q-page-v2/injured-body-parts')
     // Go to new body part section
-  } else if (mostCommonInjuries.includes('Broken bones')) {
-    res.redirect('/concepts/physical-injuries/most-common-first/most-common/broken-bones')
-  } else if (mostCommonInjuries.includes('Bruising')) {
-    res.redirect('/concepts/physical-injuries/most-common-first/most-common/bruising')
-  } else if (mostCommonInjuries.includes('Cuts or grazes')) {
-    res.redirect('/concepts/physical-injuries/most-common-first/most-common/cuts-grazes')
-  } else if (mostCommonInjuries.includes('Scarring')) {
-    res.redirect('/concepts/physical-injuries/most-common-first/most-common/scarring')
-    // Go to new body part section
   } else {
     res.redirect('/concepts/physical-injuries/most-common-first/end')
   }
