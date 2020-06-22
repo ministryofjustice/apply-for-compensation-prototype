207e8206f5f3d0fb85dcdd23d5f24b879acb6e10
Jen <hello@jenthomson.com>
2020-06-22 13:20:40 +0100

    1QPPv4 - Removed minor injuries and paralysis from start of flow

--- app/views/concepts/physical-injuries/one-q-page-v4/pregnancy-loss/routes.js
+++ app/views/concepts/physical-injuries/one-q-page-v4/pregnancy-loss/routes.js
@@ -1,7 +1,7 @@
 module.exports = function (router, content) {
 
 router.post('/concepts/physical-injuries/one-q-page-v4/pregnancy-loss', function (req, res) {
-    res.redirect('/concepts/physical-injuries/one-q-page-v4/other-injuries')
+    res.redirect('/concepts/physical-injuries/one-q-page-v4/injured-body-parts')
 })
 
 // END__######################################################################################################
