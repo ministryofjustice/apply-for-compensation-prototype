5f88a375ffd0c1a1c72c7d9b29cf46136b83bd8c
jenthomson <hello@jenthomson.com>
2020-08-12 16:18:19 +0100

    Fixes + indicent type

--- app/views/concepts/physical-injuries/one-q-page-v4/finished-treatment/routes.js
+++ app/views/concepts/physical-injuries/one-q-page-v4/finished-treatment/routes.js
@@ -10,7 +10,7 @@ router.post('/concepts/physical-injuries/one-q-page-v4/finished-treatment', func
       return res.redirect('/concepts/physical-injuries/one-q-page-v4/check-your-answers-page')
     }
     // If the variable is any other value (or is missing) render the page requested
-    res.redirect('/concepts/physical-injuries/one-q-page-v4/gp-registered')
+    res.redirect('/concepts/physical-injuries/one-q-page-v4/context-your-money')
 
 })
 
