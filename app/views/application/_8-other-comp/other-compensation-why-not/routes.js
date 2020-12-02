4429fc6b846e414e36fc324f4c4a2055a911d3ae
Jen <hello@jenthomson.com>
2020-12-02 15:25:45 +0000

    Updated the incident type page

--- app/views/application/_8-other-comp/other-compensation-why-not/routes.js
+++ app/views/application/_8-other-comp/other-compensation-why-not/routes.js
@@ -4,7 +4,7 @@ module.exports = function (router, content) {
   // Variable: otherCompensation
 
   router.post('/application/_8-other-comp/other-compensation-why-not', function (req, res) {
-    res.redirect('/application/_9-additional-info/context-additional-info')
+      res.redirect('/application/_9-additional-info/context-additional-info')
  })
 
  // Pass the question in to the page
