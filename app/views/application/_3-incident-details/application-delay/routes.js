b2dae07284ee821784b343217049aa15f603ed00
Jen Thomson <hello@jenthomson.com>
2021-03-03 19:30:23 +0000

    Content updates on parent applying for minor concepts

--- app/views/application/_3-incident-details/application-delay/routes.js
+++ app/views/application/_3-incident-details/application-delay/routes.js
@@ -4,26 +4,26 @@ module.exports = function (router, content) {
   // we see that screen if the indicent date (or when the incident stopped for POA) is over 2 years from the date of application
   //logic for it is for incident-date and period-of-abuse-end
 
-  router.post('/application/application-delay', function (req, res) {
+  router.post('/application/_3-incident-details/application-delay', function (req, res) {
     if (req.session.checking_answers) { //the user was coming from the check your answer page, we are returning them there
       return res.redirect('/application/_10-end/check-your-answers-page')
     }
-    res.redirect('/application/incident-location')
+    res.redirect('/application/_3-incident-details/incident-location')
   })
 
   // Pass the question in to the page
-  router.get('/application/application-delay/', function (req, res) {
-    res.render('application/application-delay/index', content)
+  router.get('/application/_3-incident-details/application-delay/', function (req, res) {
+    res.render('application/_3-incident-details/application-delay/index', content)
   })
 
   // Pass the error state in to the page when no checkboxes are selected
-  router.get('/application/application-delay/error-checkboxes', function (req, res) {
-    res.render('application/application-delay/error-checkboxes', content)
+  router.get('/application/_3-incident-details/application-delay/error-checkboxes', function (req, res) {
+    res.render('application/_3-incident-details/application-delay/error-checkboxes', content)
   })
 
   // Pass the error state in to the page when no text is entered in explanation field
-  router.get('/application/application-delay/error-explain', function (req, res) {
-    res.render('application/application-delay/error-explain', content)
+  router.get('/application/_3-incident-details/application-delay/error-explain', function (req, res) {
+    res.render('application/_3-incident-details/application-delay/error-explain', content)
   })
   // END__######################################################################################################
 }
--- app/views/concepts/applying-for-someone-else/minors/application/v1/_3-incident-details/application-delay/routes.js
+++ app/views/concepts/applying-for-someone-else/minors/application/v1/_3-incident-details/application-delay/routes.js
@@ -4,26 +4,26 @@ module.exports = function (router, content) {
   // we see that screen if the indicent date (or when the incident stopped for POA) is over 2 years from the date of application
   //logic for it is for incident-date and period-of-abuse-end
 
-  router.post('/concepts/applying-for-someone-else/minors/application/v1/application-delay', function (req, res) {
+  router.post('/concepts/applying-for-someone-else/minors/application/v1/_3-incident-details/application-delay', function (req, res) {
     if (req.session.checking_answers) { //the user was coming from the check your answer page, we are returning them there
       return res.redirect('/concepts/applying-for-someone-else/minors/application/v1/_10-end/check-your-answers-page')
     }
-    res.redirect('/concepts/applying-for-someone-else/minors/application/v1/incident-location')
+    res.redirect('/concepts/applying-for-someone-else/minors/application/v1/_3-incident-details/incident-location')
   })
 
   // Pass the question in to the page
-  router.get('/concepts/applying-for-someone-else/minors/application/v1/application-delay/', function (req, res) {
-    res.render('concepts/applying-for-someone-else/minors/application/v1/application-delay/index', content)
+  router.get('/concepts/applying-for-someone-else/minors/application/v1/_3-incident-details/application-delay/', function (req, res) {
+    res.render('concepts/applying-for-someone-else/minors/application/v1/_3-incident-details/application-delay/index', content)
   })
 
   // Pass the error state in to the page when no checkboxes are selected
-  router.get('/concepts/applying-for-someone-else/minors/application/v1/application-delay/error-checkboxes', function (req, res) {
-    res.render('concepts/applying-for-someone-else/minors/application/v1/application-delay/error-checkboxes', content)
+  router.get('/concepts/applying-for-someone-else/minors/application/v1/_3-incident-details/application-delay/error-checkboxes', function (req, res) {
+    res.render('concepts/applying-for-someone-else/minors/application/v1/_3-incident-details/application-delay/error-checkboxes', content)
   })
 
   // Pass the error state in to the page when no text is entered in explanation field
-  router.get('/concepts/applying-for-someone-else/minors/application/v1/application-delay/error-explain', function (req, res) {
-    res.render('concepts/applying-for-someone-else/minors/application/v1/application-delay/error-explain', content)
+  router.get('/concepts/applying-for-someone-else/minors/application/v1/_3-incident-details/application-delay/error-explain', function (req, res) {
+    res.render('concepts/applying-for-someone-else/minors/application/v1/_3-incident-details/application-delay/error-explain', content)
   })
   // END__######################################################################################################
 }
