008c9559a96a54e87bb8dd0e14fcf18e26a8a52a
murrlipp <58863567+murrlipp@users.noreply.github.com>
2021-10-11 08:36:43 +0100

    Fixed routing in minor journey

--- app/views/concepts/applying-for-someone-else/minors/application/v1/_3-incident-details/own-words/routes.js
+++ app/views/concepts/applying-for-someone-else/minors/application/v1/_3-incident-details/own-words/routes.js
@@ -5,18 +5,7 @@ module.exports = function (router, content) {
 
   router.post('/concepts/applying-for-someone-else/minors/application/v1/_3-incident-details/own-words', function (req, res) {
     // Get the answer from the query string
-    var over18 = req.session.data['over18']
-
-    if (over18 === 'No') {
-      // Redirect to the relevant page
-      res.redirect('/concepts/applying-for-someone-else/minors/application/v1/transition')
-    } else {
-      if (req.session.checking_answers) { //the user was coming from the check your answer page, we are returning them there
-        return res.redirect('/concepts/applying-for-someone-else/minors/application/v1/_10-end/check-your-answers-page')
-      }
-      // If the variable is any other value (or is missing) render the page requested
       res.redirect('/concepts/applying-for-someone-else/minors/application/v1/_4-offender/context-contact-with-offender')
-    }
   })
 
   // Pass the question in to the page
