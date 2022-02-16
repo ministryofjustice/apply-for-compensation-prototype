567ddd4da6b57f4e9f03bcdc45a28de31d22b2da
murrlipp <58863567+murrlipp@users.noreply.github.com>
2022-02-16 10:25:55 +0000

    Amend commented out CP section

--- github.tf
+++ github.tf
@@ -17,13 +17,11 @@ module "github-prototype" {
   prototype_create_docker_ignore_file = false
 */
 
-  /*
   # Provide path of files, if you want to use your own template files.
   github_workflow_content          = trimspace(".github/workflows/cd.yaml")
   dockerfile_content               = trimspace("Dockerfile")
   docker_ignore_file_content       = trimspace(".dockerignore")
   deployment_file_content          = trimspace("kubernetes-deploy.tpl")
   start_sh_file_content            = trimspace("start.sh")
-*/
 
 }
