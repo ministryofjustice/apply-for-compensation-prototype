40bd28859ee0dbc6ab7d4e5c6fb6384262064e76
murrlipp <58863567+murrlipp@users.noreply.github.com>
2022-03-15 16:04:04 +0000

    Routing fix - rep journey

--- app/views/concepts/third-party-reps/prototype/_01-victim-details/date-of-birth/they/routes.js
+++ app/views/concepts/third-party-reps/prototype/_01-victim-details/date-of-birth/they/routes.js
@@ -8,24 +8,8 @@ module.exports = function (router, content) {
     var directApplicant = req.session.data['direct-applicant']
     var over18 = req.session.data['over18']
 
-    if (directApplicant === 'Myself') {
-      // getting the inputs to be able to calculate if the user is a minor or not on the day of application
-      var year = Number.parseInt(req.session.data['dob-year'], 10); // making sure with have a well formated number for year, month and day
-      var month = Number.parseInt(req.session.data['dob-month'] - 1, 10); // month are starting at 0 in javascript, that's why we need to subtract 1
-      var day = Number.parseInt(req.session.data['dob-day'], 10);
-
-      var currentDate = moment(); //create a date that is 'just now ' so today
-      var dateOfBirth = moment([year, month, day]); //create a date that is the DOB from the 3 elements we received on the date of birth page from the user)
-
-      var duration = moment.duration(currentDate.diff(dateOfBirth));// calculate the difference between the two
-      var ageInYears = duration.asYears(); // take that number in years  - we can do that thanks to the Moment library
-
-      if(ageInYears < 18) { // it's a minor
-        res.redirect('/concepts/third-party-reps/prototype/_01-victim-details/date-of-birth/they/transition')
-        // @todo we need to build a page that explains to users that they can only use this service if they are over 18
-      } else if (over18 === 'Yes') {
+    if (over18 === 'Yes') {
         res.redirect('/concepts/third-party-reps/prototype/_01-victim-details/incapable-own-affairs')
-      }
     } else {
       res.redirect('/concepts/third-party-reps/prototype/_01-victim-details/address/they')
     }
